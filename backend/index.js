6acb34475a4c5d193f6195dff20285471233b103
tours <minhquando59@gmail.com>
2023-06-28 13:57:02 +0700

    fix lại

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
-
-import express from 'express';
-import dotenv from 'dotenv' ;
-import mongoose from 'mongoose';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import tourRoute from "./routes/tours.js";
 import userRoute from "./routes/users.js";
 import authRoute from "./routes/auth.js";
@@ -14,26 +13,22 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-    origin:true,
-    credentials:true
-}
+  origin: true,
+  credentials: true,
+};
 //database connection
 mongoose.set("strictQuery", false);
-const connect = async()=> {
-    try{
-        await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }) ;
-        console.log('MongoDB database connected');
-
-
-    }catch(err){
-        console.log('MongoDB database connected failed');
-    }
-}
-
-
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB database connected");
+  } catch (err) {
+    console.log("MongoDB database connected failed");
+  }
+};
 
 //middle ware
 app.use(express.json());
@@ -44,7 +39,7 @@ app.use("/api/v1/tours", tourRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/review", reviewRoute);
 app.use("/api/v1/booking", bookingRoute);
-app.listen(port, ()=>{
-    connect();
-    console.log("server duong listening on port", port);
-} );
+app.listen(port, () => {
+  connect();
+  console.log("server listening on port", port);
+});
